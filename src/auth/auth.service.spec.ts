96e4c6b99d7ebd97d383046423fc7d7fb3dabdbd
RashingPro <rashingpro@yandex.ru>
2025-06-17 00:50:59 +0300

    fix: auth.service test .env

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
 import { JwtModule, JwtService } from "@nestjs/jwt";
+import { ConfigModule } from "@nestjs/config";
 
 describe("AuthService", () => {
     let jwtService: JwtService;
@@ -9,14 +10,14 @@ describe("AuthService", () => {
     beforeEach(async () => {
         const app = await Test.createTestingModule({
             providers: [AuthService],
-            imports: [JwtModule.register({secret: "example-secret-key"})]
+            imports: [await ConfigModule.forRoot(), JwtModule.register({secret: process.env.JWT_SECRET})]
         }).compile()
 
         authService = app.get(AuthService);
         jwtService = app.get(JwtService)
     })
 
-    it("Test Jwt", async () => {
+    it("JwtService test", async () => {
         await jwtService.signAsync({})
     })
 })
