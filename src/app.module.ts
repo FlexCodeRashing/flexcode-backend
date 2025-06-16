80c69ba181d2b65af53a91821c9ce90d3d751ae9
RashingPro <rashingpro@yandex.ru>
2025-06-16 23:36:51 +0300

    build: implement @nestjs/jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from "@nestjs/config"
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
-    imports: [ConfigModule.forRoot()],
+    imports: [
+        ConfigModule.forRoot(),
+        JwtModule.registerAsync({
+            useFactory: () => ({ secret: process.env.JWT_SECRET })
+        })
+    ],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [AppService]
 })
 export class AppModule {}
