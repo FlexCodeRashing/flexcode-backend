b4304a1cb9f8a2febba002d0f3c3db7e038b0c80
RashingPro <rashingpro@yandex.ru>
2025-06-17 00:58:27 +0300

    chore: remove unused

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,8 +4,4 @@ import { JwtService } from "@nestjs/jwt";
 @Injectable()
 export class AuthService {
     constructor(private readonly jwtService: JwtService) {}
-
-    async test() {
-        return await this.jwtService.signAsync({a: "b"})
-    }
 }
