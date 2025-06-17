4a1bf657380ce6c64a4d4320bbafcf1890dadf93
RashingPro <rashingpro@yandex.ru>
2025-06-17 17:04:37 +0300

    feat: add can() method to Permissions

--- src/auth/auth.types.spec.ts
+++ src/auth/auth.types.spec.ts
@@ -53,5 +53,32 @@ describe("Auth types", () => {
                     .get(Permission.MODERATOR)
             ).toBe(false);
         });
+        it("should return true (can)", () => {
+            const a = new Permissions(parseInt("10101", 2));
+            const b = new Permissions(parseInt("00101", 2));
+            expect(a.can(b)).toBe(true);
+        });
+        it("should return false (can)", () => {
+            const a = new Permissions(parseInt("10101", 2));
+            const b = new Permissions(parseInt("01101", 2));
+            expect(a.can(b)).toBe(false);
+        });
+        it("should return true too (can)", () => {
+            const a = new Permissions()
+                .set(Permission.ADMIN, true)
+                .set(Permission.MODERATOR, true);
+            const b = new Permissions().set(Permission.MODERATOR, true);
+            expect(a.can(b)).toBe(true);
+        });
+        it("should return false too (can)", () => {
+            const a = new Permissions().set(Permission.MODERATOR, true);
+            const b = new Permissions().set(Permission.ADMIN, true);
+            expect(a.can(b)).toBe(false);
+        });
+        it("should return true (can 0)", () => {
+            const a = new Permissions();
+            const b = new Permissions();
+            expect(a.can(b)).toBe(true);
+        });
     });
 });
