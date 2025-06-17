4954f4371819fb9b23863433f4c062ef189ae4d9
RashingPro <rashingpro@yandex.ru>
2025-06-17 14:48:16 +0300

    refactor: lint and format code

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,44 +1,41 @@
 export class BitSet {
     constructor(value?: number | boolean[]) {
         if (value) {
-            if (typeof value == "number")
-                this.value = value;
+            if (typeof value == "number") this.value = value;
             else {
-                let bin = ""
+                let bin = "";
                 for (const i of value) {
-                    bin += i ? "1" : "0"
+                    bin += i ? "1" : "0";
                 }
-                this.value = parseInt(bin.split("").reverse().join(""), 2)
+                this.value = parseInt(bin.split("").reverse().join(""), 2);
             }
-        }
-        else
-            this.value = 0
+        } else this.value = 0;
     }
 
-    public readonly value: number
+    public readonly value: number;
 
     private _getArray(): boolean[] {
-        const bin = (this.value >>> 0).toString(2)
-        let result: boolean[] = []
+        const bin = (this.value >>> 0).toString(2);
+        const result: boolean[] = [];
         for (const i of bin) {
-            result.push(i == "1")
+            result.push(i == "1");
         }
-        return result.reverse()
+        return result.reverse();
     }
 
     set(index: number, value: boolean): BitSet {
-        const array = this._getArray()
-        while (array.length <= index)
-            array.push(false)
-        array[index] = value
-        return new BitSet(array)
+        const array = this._getArray();
+        while (array.length <= index) array.push(false);
+        array[index] = value;
+        return new BitSet(array);
     }
 
     get(index: number): boolean {
-        const array = this._getArray()
-        if (index < array.length)
-            return array[index]
+        const array = this._getArray();
+        if (index < array.length) return array[index];
         else
-            throw new RangeError(`Trying to get at index ${index} while BitSet contains only ${array.length} values`)
+            throw new RangeError(
+                `Trying to get at index ${index} while BitSet contains only ${array.length} values`
+            );
     }
 }
--- src/types/types.spec.ts
+++ src/types/types.spec.ts
@@ -3,32 +3,32 @@ import { BitSet } from "./index";
 describe("Types", () => {
     describe("BitSet", () => {
         it("should return 0", () => {
-            const bitset = new BitSet()
-            expect(bitset.value).toBe(0)
-        })
+            const bitset = new BitSet();
+            expect(bitset.value).toBe(0);
+        });
         it("should return 1", () => {
-            const bitset = new BitSet([true])
-            expect(bitset.value).toBe(1)
+            const bitset = new BitSet([true]);
+            expect(bitset.value).toBe(1);
         });
         it("should return 1 too", () => {
-            const bitset = new BitSet([true, false, false, false])
-            expect(bitset.value).toBe(1)
+            const bitset = new BitSet([true, false, false, false]);
+            expect(bitset.value).toBe(1);
         });
         it("should return 2", () => {
-            const bitset = new BitSet().set(1, true)
-            expect(bitset.value).toBe(2)
+            const bitset = new BitSet().set(1, true);
+            expect(bitset.value).toBe(2);
         });
         it("should return 10", () => {
-            const bitset = new BitSet().set(1, true).set(3, true)
-            expect(bitset.value).toBe(10)
+            const bitset = new BitSet().set(1, true).set(3, true);
+            expect(bitset.value).toBe(10);
         });
         it("should return true", () => {
-            const bitset = new BitSet().set(1, true).set(3, true)
-            expect(bitset.get(3)).toBe(true)
+            const bitset = new BitSet().set(1, true).set(3, true);
+            expect(bitset.get(3)).toBe(true);
         });
         it("should throw an error", () => {
-            const bitset = new BitSet().set(1, true).set(3, true)
-            expect(() => bitset.get(4)).toThrow()
+            const bitset = new BitSet().set(1, true).set(3, true);
+            expect(() => bitset.get(4)).toThrow();
         });
-    })
-})
+    });
+});
