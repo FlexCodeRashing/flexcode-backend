0ddf67f5833085bfb4ff69ed392602a0b5b99093
RashingPro <rashingpro@yandex.ru>
2025-07-02 11:03:42 +0300

    feat(auth): add auth functionality to AuthService
    Add `generateSessionTokens()` and `createSession()` methods

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 
+import { PrismaService } from "../prisma/prisma.service";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 
 @Module({
     controllers: [AuthController],
-    providers: [AuthService],
+    providers: [AuthService, PrismaService],
     imports: [
         JwtModule.registerAsync({
             useFactory: () => ({ secret: process.env.JWT_SECRET })
